2ecddea1eba2d843efa452b7e9ecf59c2cb9e9a6
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-07 16:06:21 -0300

    first commit

---
+++ src/main/java/io/github/aplaraujo/DesafioTecnicoSimplifyBackendJuniorApplication.java
@@ -0,0 +1,13 @@
+package io.github.aplaraujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoSimplifyBackendJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoSimplifyBackendJuniorApplication.class, args);
+	}
+
+}
