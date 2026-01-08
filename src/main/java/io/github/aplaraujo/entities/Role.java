860e155d84a9f792fd5bc8a4d855aabf2a014d4d
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2026-01-07 21:29:33 -0300

    Autentication request entity

--- src/main/java/io/github/aplaraujo/entities/Role.java
+++ src/main/java/io/github/aplaraujo/entities/Role.java
@@ -2,9 +2,17 @@ package io.github.aplaraujo.entities;
 
 import io.github.aplaraujo.entities.enums.RoleEnum;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "tb_role")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
